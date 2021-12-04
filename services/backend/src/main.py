4c7728185576d553a809cef6bcc94177b330d053
koad7 <kodjo.adanledji@gmail.com>
2021-12-04 20:45:14 +0100

    Vue added

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,8 +1,17 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # NEW
 
 
 app = FastAPI()
+
+# NEW
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
