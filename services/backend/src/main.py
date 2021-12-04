766ce1599c3715762f97276f8bf3b4e34f29e134
koad7 <kodjo.adanledji@gmail.com>
2021-12-04 21:27:01 +0100

    Database added

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # NEW
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.database.register import register_tortoise  # NEW
+from src.database.config import TORTOISE_ORM         # NEW
 
 
 app = FastAPI()
 
-# NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# NEW
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get("/")
