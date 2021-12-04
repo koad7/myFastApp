71ee497f0d53c3a3a7fc86619d4c86e9b99736ea
koad7 <kodjo.adanledji@gmail.com>
2021-12-04 22:06:34 +0100

    CRUD

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from tortoise import Tortoise  # NEW
 
-from src.database.register import register_tortoise  # NEW
-from src.database.config import TORTOISE_ORM         # NEW
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
 
+
+# enable schemas to read relationship between models
+Tortoise.init_models(["src.database.models"], "models")  # NEW
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# NEW
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
